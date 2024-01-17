3cbf062a588acc71d9921d5228b530f8bf56c485
pietervdvn <pietervdvn@posteo.net>
2024-01-17 17:28:46 +0000

    Deploying a new version of mapcomplete

--- mc/feature/velopark/assets/test-20befc40.js
+++
@@ -1,2 +0,0 @@
-import"./Utils-498bb79d.js";import{S as s,i as a,s as c,c as i,m,n as p,t as f,g as l,j as u,k as _}from"./index-f508aba0.js";import{c as $}from"./Icon-c5c3009e.js";import"./tw-merge-2cd9e3ed.js";function g(o){let t,e;return t=new $({props:{clss:"h-16 w-16",icon:"heart",color:"#ff0000"}}),{c(){i(t.$$.fragment)},m(n,r){m(t,n,r),e=!0},p,i(n){e||(f(t.$$.fragment,n),e=!0)},o(n){l(t.$$.fragment,n),e=!1},d(n){u(t,n)}}}class d extends s{constructor(t){super(),a(this,t,null,g,c,{})}}new _(d,{}).AttachTo("maindiv");
-//# sourceMappingURL=test-20befc40.js.map
--- mc/feature/velopark/service-worker.js
+++ mc/feature/velopark/service-worker.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var _this = this;
-var version = "0.0.8-5a48a2e19cdd6449756b982a2b92f433869852db";
+var version = "0.0.8-8bcc8820acf8cc6d3e311f3ee53d04db2b2df187";
 function install() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
